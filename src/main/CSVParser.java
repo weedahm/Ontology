cf97d719d98b1c5112718354a42b516dbc16ff99
taewoo <cow9404@gmail.com>
2018-07-26 15:16:37 +0900

    Setup Maven

---
+++ src/main/CSVParser.java
@@ -0,0 +1,30 @@
+package main;
+
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+ 
+import com.opencsv.CSVReader;
+ 
+public class CSVParser {
+ 
+    public static void main(String[] args) {
+        try{
+         InputStreamReader is = new InputStreamReader(new FileInputStream("C:\\Users\\cow94\\Desktop\\last.csv"), "EUC-KR");
+         CSVReader reader = new CSVReader(is);
+         List<String[]> list = reader.readAll();
+         
+         for(String[] str : list){
+             System.out.println();
+             for(String s : str){
+                 System.out.print(s + " ");
+             }
+         }
+         
+         
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+ 
+}
